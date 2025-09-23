0be8d3cdbff463bbc1b810f82fc0268e919b6c1a
Samuel12372 <c2028335@hallam.shu.ac.uk>
2025-09-23 22:52:17 +0100

    Update GlowingCursor.jsx

--- client/src/components/GlowingCursor.jsx
+++ client/src/components/GlowingCursor.jsx
@@ -8,14 +8,40 @@ const GlowingCursor = () => {
       setPosition({ x: e.clientX, y: e.clientY });
     };
 
-    window.addEventListener("mousemove", updatePosition);
-    return () => window.removeEventListener("mousemove", updatePosition);
+    // Only add mouse listener on large screens
+    const mediaQuery = window.matchMedia('(min-width: 1024px)');
+    
+    const handleScreenSize = () => {
+      if (mediaQuery.matches) {
+        window.addEventListener("mousemove", updatePosition);
+      } else {
+        window.removeEventListener("mousemove", updatePosition);
+      }
+    };
+
+    handleScreenSize();
+    mediaQuery.addEventListener('change', handleScreenSize);
+
+    return () => {
+      window.removeEventListener("mousemove", updatePosition);
+      mediaQuery.removeEventListener('change', handleScreenSize);
+    };
   }, []);
 
   return (
-    <div className="pointer-events-none fixed top-0 left-0 w-full h-full z-50 hidden lg:block">
+    <div className="pointer-events-none fixed top-0 left-0 w-full h-full z-50">
       <div
-        className="absolute bg-gray-200 rounded-full opacity-20 blur-[100px] transition-transform duration-50"
+        className="absolute bg-gray-200 rounded-full opacity-20 blur-[100px] transition-transform duration-50 lg:hidden"
+        style={{
+          width: "500px",
+          height: "500px",
+          left: "50%",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      />
+      <div
+        className="absolute bg-gray-200 rounded-full opacity-20 blur-[100px] transition-transform duration-50 hidden lg:block"
         style={{
           width: "500px",
           height: "500px",
